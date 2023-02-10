f87ce393ee494da6f5279d5c494a1443a2daa016
xzqt23 <angelo.agualada@gmail.com>
2023-02-10 19:52:01 +0800

    Create math.cpp

---
+++ math.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+# include <cmath> // use this header file to use other math functions
+int main ()
+
+{
+    double x = 4;
+    double y = 6;
+    double z;
+    double a = 3.14;
+    double b = 3.99;
+
+    // z = std::max(x,y); 
+    // z = std::min(x,y); 
+    // z = pow(2,3);
+    // z = sqrt(64);
+    // z = abs(-4);
+    // z = round(a);
+    // z = ceil(a);
+    z = floor(b);
+
+    std::cout << z;
+    return 0;
+}
