9cee38a9def9a9e7a6af814835946cfb3e164ede
xzqt23 <angelo.agualada@gmail.com>
2023-02-15 13:30:31 +0800

    Create nestedLoop.cpp

---
+++ nestedLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{   
+    for(int x = 1;x <= 3;x++){
+
+        for(int j = 1; j <= 10;j++)
+         {
+        std::cout<<j<<" ";
+         }
+        
+        std::cout<<"\n";
+    }
+
+   
+    return 0;
+}
